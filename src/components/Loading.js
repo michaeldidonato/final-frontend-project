94ecfbdc17ab34ec199b623b9b0c6ece390c7ac7
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2022-02-21 18:51:15 +0100

    Create Loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Loading() {
+  return (
+      <>
+        <div class="spinner-border text-warning m-3" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+      </>
+    
+  )
+}
+
+export default Loading;
