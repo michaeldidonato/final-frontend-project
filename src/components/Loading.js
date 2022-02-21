4954797041e412cf9235024d17613c4c38dbb7a6
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2022-02-21 19:02:47 +0100

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function Loading() {
   return (
-      <>
-        <div class="spinner-border text-warning m-3" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-      </>
-    
-  )
+    <>
+      <div class="spinner-border text-warning m-3" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </>
+  );
 }
 
 export default Loading;
