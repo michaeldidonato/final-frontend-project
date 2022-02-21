fd89df3b41509c6dbcd6ea9a5d387a788cd3aba1
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2022-02-21 19:02:40 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,13 +33,12 @@ function Home() {
 
   return (
     <main>
-
       <div className={styles["Header"]}>
         <div className={styles["Header-background"]}></div>
         <div className={styles["Header-content"]}>
           <div className={styles["Header-hero"]}>
             <h1>
-              Veg Recipes App <br /> 
+              Veg Recipes App <br />
             </h1>
             <p>
               Tired to looking for the same recipes every day?
@@ -57,23 +56,29 @@ function Home() {
           <div className="col-lg-4 p-3">
             <RiPlantFill className={styles["icon"]} />
             <h2>Titolo 1</h2>
-            <p className="lead mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            <p className="lead mb-0">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
             </p>
           </div>
           <div className={`col-lg-4 p-3`}>
-          <BsFillSunFill className={styles["icon"]} />
-          <h2>Titolo 2</h2>
-            <p className="lead mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            <BsFillSunFill className={styles["icon"]} />
+            <h2>Titolo 2</h2>
+            <p className="lead mb-0">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
             </p>
           </div>
 
           <div className="col-lg-4 p-3">
-          <FaLeaf className={styles["icon"]} />
-          <h2>Titolo 3</h2>
-            <p className="lead mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            <FaLeaf className={styles["icon"]} />
+            <h2>Titolo 3</h2>
+            <p className="lead mb-0">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
             </p>
           </div>
         </div>
@@ -86,19 +91,20 @@ function Home() {
           <>
             <h2 className="mt-2 mb-2">Highlights</h2>
             <div className="container-fluid mx-auto row">
-            {randomRecipes.map((item) => (
-              
-                
-                <div className="card col-lg-4"  key={item?.id}>
-                <img src={item?.image} className="card-img-top" alt={item?.title} />
+              {randomRecipes.map((item) => (
+                <div className="card col-lg-4" key={item?.id}>
+                  <img
+                    src={item?.image}
+                    className="card-img-top"
+                    alt={item?.title}
+                  />
                   <div className="card-body">
-                  <Link to={`/recipe-list/${item?.id}`}>
-                    <h3 className="card-title">{item?.title}</h3>
-                  </Link>  
+                    <Link to={`/recipe-list/${item?.id}`}>
+                      <h3 className="card-title">{item?.title}</h3>
+                    </Link>
                   </div>
                 </div>
-                
-            ))}
+              ))}
             </div>
           </>
         )}
