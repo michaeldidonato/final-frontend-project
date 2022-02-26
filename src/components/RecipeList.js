c29e6e2ec40f98fec0497320643de589b4e2ce95
michaeldidonato <michaeldd97@gmail.com>
2022-02-26 19:30:58 +0100

    Update RecipeList.js

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -29,10 +29,9 @@ function RecipeList() {
   };
 
   return (
-    <div>
-      
+    <section>
       <div className={`container-fluid mx-auto ${styles["search-box"]}`}>
-      <h1 className=" pb-3 text-white">Type a Recipe you need</h1>
+        <h1 className=" pb-3 text-white">Type a Recipe you need</h1>
         <form onSubmit={searchRecipes}>
           <input
             type="text"
@@ -47,26 +46,37 @@ function RecipeList() {
         </form>
       </div>
 
-      <div className={`container-fluid mx-auto row pt-4 pb-3 ${styles["bg-color"]}`}>
-        {boxItems.map((item) => {
-          return (
-            <div className="card col-lg-4 mb-4" key={item?.id}>
-              <img
-                src={item?.image}
-                className={`rounded mx-auto d-block ${styles["img-size"]}`}
-                alt={item?.title}
-              />
-              <div className="card-body">
-                  <h3 className="card-title">{item?.title}</h3>
-                <Link to={`/recipe-list/${item?.id}`}>
-                <button class="mt-3 btn btn-warning">Go somewhere</button>
-                </Link>
-              </div>
-            </div>
-          );
-        })}
+      <div
+        className={`container-fluid mx-auto row pt-4 pb-3 ${styles["bg-color"]}`}
+      >
+        {!boxItems.length ? (
+          <h3 className={styles["undefined-recipes"]}>
+            Nothing found <br />
+            try to search something else
+          </h3>
+        ) : (
+          <>
+            {boxItems.map((item) => {
+              return (
+                <div className="card col-lg-4 mb-4" key={item?.id}>
+                  <img
+                    src={item?.image}
+                    className={`rounded mx-auto d-block ${styles["img-size"]}`}
+                    alt={item?.title}
+                  />
+                  <div className="card-body">
+                    <h3 className="card-title">{item?.title}</h3>
+                    <Link to={`/recipe-list/${item?.id}`}>
+                      <button class="mt-3 btn btn-warning">Read more</button>
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
-    </div>
+    </section>
   );
 }
 
