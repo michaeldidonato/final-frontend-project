8b1c119683f27c4b0cd2c8ce10197a489efd1240
michaeldidonato <michaeldd97@gmail.com>
2022-02-27 19:49:49 +0100

    Update SingleRecipe.js

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -38,33 +38,51 @@ function SingleRecipe() {
         <div className={`container-fluid`}>
           <h1 className="text-center p-2">{item?.title}</h1>
           <div className={styles["img-center"]}>
-
-          <img
-            className={`${styles["img-size"]} rounded pb-4`}
-            src={item?.image}
-            alt={item?.title}
-          />
+            <img
+              className={`${styles["img-size"]} mb-4`}
+              src={item?.image}
+              alt={item?.title}
+            />
           </div>
-          <div className="container mx-auto">
+
+          <div className={`container mx-auto p-2 ${styles["bg-summary"]}`}>
             <Markup content={contentArticle} />
           </div>
 
-          {item?.extendedIngredients.map(ing => {
-            return(
-             <div>
-               <img src={ing?.image} alt="" />
-               <p>{ing?.name}</p>
-             </div> 
-            )
-          })}
+          <div className="container mx-auto">
+            <>
+              <h2 className="text-center mt-2 pt-4">Ingredients</h2>
+            </>
+            <div className={styles["ingredients"]}>
+              {item?.extendedIngredients.map((ing) => {
+                return (
+                  <div className="col-md-2 m-2 p-2" key={ing?.id}>
+                    <img
+                      className={styles["center"]}
+                      src={`https://spoonacular.com/cdn/ingredients_100x100/${ing?.image}`}
+                      alt={ing?.name}
+                    />
+                    <p className={styles["center"]}>
+                      {ing?.name} {ing?.measures?.metric?.amount}{" "}
+                      {ing?.measures?.metric?.unitShort}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
 
+          <section className="container mx-auto">
+            <h2 className="pt-3 pb-1">Instructions</h2>
+            <div className={`mb-3`}>
+              <Markup
+                className="d-flex align-content-start"
+                content={item?.instructions}
+              />
+            </div>
+          </section>
         </div>
-
-
       )}
-
-        
-
     </>
   );
 }
