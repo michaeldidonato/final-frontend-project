3eace069369614ba80d030e7ba5fb91843f2d832
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2022-02-28 09:02:38 +0100

    Update RecipeList.js

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -77,7 +77,7 @@ function RecipeList() {
                   <div className="card-body">
                     <h3 className="card-title">{item?.title}</h3>
                     <Link to={`/recipe-list/${item?.id}`}>
-                      <button class="mt-3 btn btn-warning">Read more</button>
+                      <button className="mt-3 btn btn-warning">Read more</button>
                     </Link>
                   </div>
                 </div>
